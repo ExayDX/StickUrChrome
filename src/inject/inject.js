5bfd3946b4aff25d5afb9f7d5b97b6d1c7bdab06
Nicolas Lupien <niclupien@gmail.com>
2013-08-17 15:46:26 -0400

    storing stickers

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,9 +1,38 @@
+var gstickers = new Array();
+
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
-  console.log(request.imgUrl);
 
   var img = document.createElement('img');
   img.setAttribute('src', request.imgUrl);
   img.setAttribute('class', "sticker");
   $("body").prepend(img);
   $(".sticker").draggable();
+
+  var obj = {url: request.imgUrl};
+
+  gstickers.push(obj);
+
+  chrome.storage.sync.set({'stickers': gstickers}, function() {
+    console.log('Settings saved');
+  });
+
+});
+
+$(document).ready(function(){
+
+  chrome.storage.sync.get('stickers', function(obj){;
+
+    if (!jQuery.isEmptyObject(obj)) {
+
+      gstickers = obj.stickers;
+
+      for (var i = 0; i < gstickers.length; i++) {
+        var img = document.createElement('img');
+        img.setAttribute('src', gstickers[i].url);
+        img.setAttribute('class', "sticker");
+        $("body").prepend(img);
+        $(".sticker").draggable();
+      }
+    }
+  });
 });
