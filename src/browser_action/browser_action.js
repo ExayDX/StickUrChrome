459b4088c6161b9ad8aaea02514525fa296ccf75
Nicolas Lupien <niclupien@gmail.com>
2013-08-17 16:42:58 -0400

    position

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,9 +1,11 @@
 function add_sticker(e) {
   //var imgUrl = chrome.extension.getURL("../../img/stickers/github.png");
   var imgUrl = chrome.extension.getURL(e.currentTarget.getAttribute('src'));
+  var txtbox = document.getElementById("sticker_text");
+
 
   chrome.tabs.getSelected(null, function(tab) {
-    chrome.tabs.sendMessage(tab.id, {'imgUrl': imgUrl});
+    chrome.tabs.sendMessage(tab.id, {'imgUrl': imgUrl, 'content': txtbox.value});
   });
 
   window.close();
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,13 @@
+function save_position(e) {
+  console.log(e);
+  var i = $(e.target).data('stickid');
+  gstickers[i].x = e.pageX - e.offsetX;
+  gstickers[i].y = e.pageY - e.offsetY;
+  chrome.storage.sync.set({'stickers': gstickers}, function() {
+    console.log('Settings saved');
+  });
+}
+
 var gstickers = new Array();
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
@@ -5,10 +15,15 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
   var img = document.createElement('img');
   img.setAttribute('src', request.imgUrl);
   img.setAttribute('class', "sticker");
+  img.setAttribute('data-stickid', gstickers.length);
+  img.setAttribute('title', request.content);
   $("body").prepend(img);
-  $(".sticker").draggable();
+  $(".sticker").draggable({
+    stop: save_position,
+    containment: "body"
+  });
 
-  var obj = {url: request.imgUrl};
+  var obj = {id: gstickers.length,url: request.imgUrl, location: window.location.href, content: request.content};
 
   gstickers.push(obj);
 
@@ -19,20 +34,32 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
 });
 
 $(document).ready(function(){
-
+  //chrome.storage.sync.remove('stickers');
   chrome.storage.sync.get('stickers', function(obj){;
 
     if (!jQuery.isEmptyObject(obj)) {
 
       gstickers = obj.stickers;
+      console.log(gstickers);
 
       for (var i = 0; i < gstickers.length; i++) {
-        var img = document.createElement('img');
-        img.setAttribute('src', gstickers[i].url);
-        img.setAttribute('class', "sticker");
-        $("body").prepend(img);
-        $(".sticker").draggable();
+        console.log(gstickers[i].location);
+        if (gstickers[i].location == window.location.href) {
+          var img = document.createElement('img');
+          img.setAttribute('src', gstickers[i].url);
+          img.setAttribute('class', "sticker");
+          img.setAttribute('title', gstickers[i].content);
+          img.setAttribute('data-stickid', gstickers[i].id);
+          $(img).css('left', gstickers[i].x);
+          $(img).css('top', gstickers[i].y);
+          $("body").prepend(img);
+          $(".sticker").draggable({
+            stop: save_position,
+            containment: "body"
+          });
+        }
       }
     }
+    $( document ).tooltip();
   });
 });
