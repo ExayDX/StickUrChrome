5e9b90b84dac68449139949863ea1f1a24b3139c
Nicolas Lupien <niclupien@gmail.com>
2013-08-17 17:07:46 -0400

    code cleaning

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,65 +1,98 @@
+// Variable globale qui contient tous les stickers du localStorage
+var gstickers = new Array();
+
+/**
+ * Sauvegarde la position d'un sicker
+ */
 function save_position(e) {
-  console.log(e);
   var i = $(e.target).data('stickid');
   gstickers[i].x = e.pageX - e.offsetX;
   gstickers[i].y = e.pageY - e.offsetY;
+  chrome.storage.sync.set({'stickers': gstickers});
+}
+
+/**
+ * Reçoit un message du browser action comme quoi un nouveau sticker
+ * a été ajouté
+ */
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
+  // On crée l'objet sticker
+  var sticker = {
+    id: gstickers.length,
+    url: request.imgUrl, 
+    location: window.location.href, 
+    content: request.content };
+
+  // Ajout du sticker
+  var el = create_sticker_el(sticker);
+  $("body").prepend(el);
+
+  // Sauvegarde du sticker
+  save_sticker(sticker);
+
+  // Setup du drag/drop
+  $(".sticker").draggable({
+    stop: save_position,
+    containment: "body"
+  });
+});
+
+function save_sticker(sticker) {
+  gstickers.push(sticker);
+
   chrome.storage.sync.set({'stickers': gstickers}, function() {
     console.log('Settings saved');
   });
 }
 
-var gstickers = new Array();
+// Créer un element html représentant un sticker
+function create_sticker_el(sticker) {
+  var img = $("<img />");
+  img.attr('src', sticker.url);
+  img.attr('class', "sticker");
+  img.attr('title', sticker.content);
+  img.attr('data-stickid', sticker.id);
+  img.css('left', sticker.x);
+  img.css('top', sticker.y);
+  return img;
+}
 
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
-
-  var img = document.createElement('img');
-  img.setAttribute('src', request.imgUrl);
-  img.setAttribute('class', "sticker");
-  img.setAttribute('data-stickid', gstickers.length);
-  img.setAttribute('title', request.content);
-  $("body").prepend(img);
-  $(".sticker").draggable({
-    stop: save_position,
-    containment: "body"
-  });
-
-  var obj = {id: gstickers.length,url: request.imgUrl, location: window.location.href, content: request.content};
-
-  gstickers.push(obj);
-
-  chrome.storage.sync.set({'stickers': gstickers}, function() {
-    console.log('Settings saved');
-  });
-
-});
+// Affiche les stickers passés en paramètres
+function display_stickers(stickers) {
+  for (var i = 0; i < gstickers.length; i++) {
+    if (gstickers[i].location == window.location.href) {
+      var el = create_sticker_el(gstickers[i]);
+      $("body").prepend(el);
+    }
+  }
+}
 
 $(document).ready(function(){
-  //chrome.storage.sync.remove('stickers');
+  // uncomment to remove all sitckers from localstorage
+  // chrome.storage.sync.remove('stickers');
+
+  // Récupératio des stickers dans le localstorage
   chrome.storage.sync.get('stickers', function(obj){;
 
+    // On traite seulement s'il y a des stickers de sauvegardé
     if (!jQuery.isEmptyObject(obj)) {
 
+      // On assigne enregistre le contenu du localstorage dans la 
+      // variable globale gstickers
       gstickers = obj.stickers;
-      console.log(gstickers);
 
-      for (var i = 0; i < gstickers.length; i++) {
-        console.log(gstickers[i].location);
-        if (gstickers[i].location == window.location.href) {
-          var img = document.createElement('img');
-          img.setAttribute('src', gstickers[i].url);
-          img.setAttribute('class', "sticker");
-          img.setAttribute('title', gstickers[i].content);
-          img.setAttribute('data-stickid', gstickers[i].id);
-          $(img).css('left', gstickers[i].x);
-          $(img).css('top', gstickers[i].y);
-          $("body").prepend(img);
-          $(".sticker").draggable({
-            stop: save_position,
-            containment: "body"
-          });
-        }
-      }
+      // affichage des stickers
+      display_stickers(gstickers);
+
     }
+
+    // Setup des tooltips
     $( document ).tooltip();
+
+    // Setup du drag/drop
+    $(".sticker").draggable({
+      stop: save_position,
+      containment: "body"
+    });
   });
 });
