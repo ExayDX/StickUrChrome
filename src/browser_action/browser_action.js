35a53813aa196f09a57bc11f46b5b7d769485dd5
Nicolas Lupien <niclupien@gmail.com>
2013-08-17 14:16:13 -0400

    adding pix to pages

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,12 +1,18 @@
-function click(e) {
-  console.log("adding a sticker");
-  chrome.tabs.executeScript(null, {code:"document.body.style.background='red'"});
+function add_sticker(e) {
+  //var imgUrl = chrome.extension.getURL("../../img/stickers/github.png");
+  var imgUrl = chrome.extension.getURL(e.currentTarget.getAttribute('src'));
+
+  chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.sendMessage(tab.id, {'imgUrl': imgUrl});
+  });
+
   window.close();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   var imgs = document.querySelectorAll('img');
+
   for (var i = 0; i < imgs.length; i++) {
-    imgs[i].addEventListener('click', click);
+    imgs[i].addEventListener('click', add_sticker);
   }
 });
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,7 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
+  console.log(request.imgUrl);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
-	}
-	}, 10);
+  var img = document.createElement('img');
+  img.setAttribute('src', request.imgUrl);
+  document.body.appendChild(img);
 });
